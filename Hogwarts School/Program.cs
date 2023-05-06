69d016fbb2b84130bb4c9b8a5a0dda9eac83618e
mohammadaminnasiri131383@gmail.com <mohammadaminnasiri131383@gmail.com>
2023-05-06 16:15:38 +0330

    modifying folders

---
+++ Hogwarts School/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Collections;
+using System;
+using System.Threading;
+using System.Linq;
+using System.Text;
+
+namespace Hogwarts
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("HW");
+        }
+    }
+}
